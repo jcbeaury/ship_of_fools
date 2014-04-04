2379da51626574c4874a2f85f2a0b21abe8ad2f2
jcbeaury <jcbeaury@gmail.com>
2014-04-04 01:33:43 -0400

    Add formatting to cast view

--- app.rb
+++ app.rb
@@ -1,36 +1,36 @@
 require 'sinatra'
 
 get '/' do
-  @title = 'Home'
+  @title = 'Ship of Fools'
   erb :home
 end
 
 get '/about' do
-  @title = 'About'
+  @title = 'Ship of Fools | About'
   erb :'/about'
 end
 
 get '/photography' do
-  @title = 'Photography'
+  @title = 'Ship of Fools | Photography'
   erb :'/photography'
 end
 
 get '/cast' do
-  @title = 'Cast'
+  @title = 'Ship of Fools | Cast'
   erb :'/cast'
 end
 
 get '/filmmakers' do
-  @title = 'Filmmakers'
+  @title = 'Ship of Fools | Filmmakers'
   erb :'/filmmakers'
 end
 
 get '/music' do
-  @title = 'Music'
+  @title = 'Ship of Fools | Music'
   erb :'/music'
 end
 
 get '/credits' do
-  @title = 'Credits'
+  @title = 'Ship of Fools | Credits'
   erb :'/credits'
 end

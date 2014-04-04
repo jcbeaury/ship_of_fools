d85307faa897ec1f628539d7a8251a822aafade1
jcbeaury <jcbeaury@gmail.com>
2014-04-04 00:06:52 -0400

    Add header with nav bar

--- app.rb
+++ app.rb
@@ -4,3 +4,33 @@ get '/' do
   @title = 'Home'
   erb :home
 end
+
+get '/about' do
+  @title = 'About'
+  erb :'/about'
+end
+
+get '/photography' do
+  @title = 'Photography'
+  erb :'/photography'
+end
+
+get '/cast' do
+  @title = 'Cast'
+  erb :'/cast'
+end
+
+get '/filmmakers' do
+  @title = 'Filmmakers'
+  erb :'/filmmakers'
+end
+
+get '/music' do
+  @title = 'Music'
+  erb :'/music'
+end
+
+get '/credits' do
+  @title = 'Credits'
+  erb :'/credits'
+end

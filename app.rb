3daafcd403d369acaaa3f1326060cf694442a725
jcbeaury <jcbeaury@gmail.com>
2014-04-06 23:49:28 -0400

    Add functional gallery on photography page

--- app.rb
+++ app.rb
@@ -2,42 +2,42 @@ require 'sinatra'
 
 get '/' do
   @title = 'Ship of Fools'
-  @navbar1 = 'active'
+  @home = 'active'
   erb :home
 end
 
 get '/about' do
   @title = 'Ship of Fools | About'
-  @navbar2 = 'active'
+  @about = 'active'
   erb :'/about'
 end
 
 get '/photography' do
   @title = 'Ship of Fools | Photography'
-  @navbar3 = 'active'
+  @photography = 'active'
   erb :'/photography'
 end
 
 get '/cast' do
   @title = 'Ship of Fools | Cast'
-  @navbar4 = 'active'
+  @cast = 'active'
   erb :'/cast'
 end
 
 get '/filmmakers' do
   @title = 'Ship of Fools | Filmmakers'
-  @navbar5 = 'active'
+  @filmmakers = 'active'
   erb :'/filmmakers'
 end
 
 get '/music' do
   @title = 'Ship of Fools | Music'
-  @navbar6 = 'active'
+  @music = 'active'
   erb :'/music'
 end
 
 get '/credits' do
   @title = 'Ship of Fools | Credits'
-  @navbar7 = 'active'
+  @credits = 'active'
   erb :'/credits'
 end
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,14 @@
 // keep the sidebar in place
 $('#sidebar').affix({
 });
+
+// image gallery
+$(function () {
+    'use strict';
+
+    $('#image-gallery-button').on('click', function (event) {
+        event.preventDefault();
+        blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
+    });
+
+});
